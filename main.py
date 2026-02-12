924978d8c9bc2e59be257d128a8ab0fa45a27c93
Nicolas Rock <nicolasrpessoa@gmail.com>
2026-02-12 17:16:02 -0300

    Criando um .env para segurança do local dos arquivos

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('começo')
+from pdf_reader import extrair_dados_pdf
+from excel_reader import ler_planilha
+from report_generator import gerar_relatorio
+
+PDF_PATH = ""
+EXCEL_PATH = ""
+SAIDA = ""
+
+def main():
+    print("📄 Lendo PDF...")
+    texto_pdf = extrair_dados_pdf(PDF_PATH)
+
+    print("📊 Lendo planilha...")
+    planilha = ler_planilha(EXCEL_PATH)
+
+    resulmo_planilha = planilha.describe().to_string()
+
+    relatorio = f""
+
+    {texto_pdf}
+
+    {resulmo_planilha}
+    ""
+
+    print("📝 Gerando relatório...")
+    gerar_relatorio(relatorio, SAIDA)
+    
+    print("✅ Relatório criado com sucesso!")
+
+if __name__ == "__main__":
+    main()
